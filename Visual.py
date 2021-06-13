91b9370f602b85117971ec0d77843bc82d63be2f
jimmydemx <j_demx@yahoo.com>
2021-06-14 00:17:40 +0200

    visualize

--- Visual.py
+++ Visual.py
@@ -9,7 +9,12 @@ app=Flask(__name__)
 @app.route('/')
 def show():
     # read data and override the template
+    # data=
     return render_template("index.html")
-
+# 可乐鸡翅         257
+# 西红柿炒鸡蛋       256
+# 糖醋排骨         254
+# 红烧茄子         252
+# 酸辣土豆丝        250
 if __name__=="__main__":
     app.run()
--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,24 @@
 from db import db_douguo_mongo
 import pandas as pd
 
-df = pd.read_csv('')
+### 分析那种菜谱做的人最多:
+df = pd.read_csv('./douguo_table.csv')
 
+# df2 = df.recipe_title.value_counts()
 
+# print(df2)
+### 结果是：
+# 可乐鸡翅         257
+# 西红柿炒鸡蛋       256
+# 糖醋排骨         254
+# 红烧茄子         252
+# 酸辣土豆丝        250
 
+print(df.practiced. max())
 
+print(df[df['practiced'].isin([df.practiced. max()])])
 
-
+# 120  206667   stta小铭  可乐鸡翅[简单到没下过厨也会做]      17001   642417   切墩(初级)   10~30分钟   4.8
 
 
 
